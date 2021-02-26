f95938b392970978f0aff281d45e2143b1d517e9
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2021-02-26 14:30:51 -0500

    Create Programs-with-harder-Input-and-Output-2.py

---
+++ Programs-with-harder-Input-and-Output-2.py
@@ -0,0 +1,4 @@
+# Name: joe richter
+# Date: Feb. 26, 21 - Unknown
+# Programs-with-harder-Input-and-Output-2.py
+# Dirxns: Input the 6 constants for system Ax + By = C and Dx + Ey = F. Output the (x, y) solution. 
