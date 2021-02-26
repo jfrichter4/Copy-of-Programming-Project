f699a5c080425bd468874f4b2a08468c7464996d
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2021-02-26 14:54:49 -0500

    Updated from Brython Server: 2/26/2021 2:54:48 PM

--- Programs-with-harder-Input-and-Output-1.py
+++ Programs-with-harder-Input-and-Output-1.py
@@ -1,3 +1,14 @@
 # Name: joe richter
 # Date: Feb. 26, 21 - Unknown
-# Programs-with-harder-Input-and-Output
+# Programs-with-harder-Input-and-Output-1.py
+# Dirxns: Input 2 pts and a 3rd x-val. Output the 3rd y-val if the pts are on an exponential fnxn. 
+x1 = input("Input the x-value of the first point: ")
+y1 = input("Input the y-value of the first point: ")
+x2 = input("Input the x-value of the second point: ")
+y2 = input("Input the y-value of the second point: ")
+x3 = input("Input the x-value of a third point: ")
+# print("pt1: ", pt1, ", ", "pt2: ", pt2, ", ", "x3: ", x3)
+a1 = (y1/(b1**x1))
+eq1 = (a1*(b2**x2))
+print(a1)
+print(eq1)
