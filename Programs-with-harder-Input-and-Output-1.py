dde0ffd21cd8830493bd540800619e955a0bcec2
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2021-03-13 16:56:52 -0500

    Update Programs-with-harder-Input-and-Output-1.py

--- Programs-with-harder-Input-and-Output-1.py
+++ Programs-with-harder-Input-and-Output-1.py
@@ -2,13 +2,27 @@
 # Date: Feb. 26, 21 - Unknown
 # Programs-with-harder-Input-and-Output-1.py
 # Dirxns: Input 2 pts and a 3rd x-val. Output the 3rd y-val if the pts are on an exponential fnxn. 
-x1 = input("Input the x-value of the first point: ")
-y1 = input("Input the y-value of the first point: ")
-x2 = input("Input the x-value of the second point: ")
-y2 = input("Input the y-value of the second point: ")
-x3 = input("Input the x-value of a third point: ")
-# print("pt1: ", pt1, ", ", "pt2: ", pt2, ", ", "x3: ", x3)
-a1 = (y1/(b1**x1))
-eq1 = (a1*(b2**x2))
-print(a1)
-print(eq1)
+import math
+x1 = float(input("Input the x-value of the first point: "))
+y1 = float(input("Input the y-value of the first point: "))
+x2 = float(input("Input the x-value of the second point: "))
+y2 = float(input("Input the y-value of the second point: "))
+x3 = float(input("Input the x-value of a third point: "))
+#ty1 = ((y2/y1)/y1)
+#tx1 = (x2-x1)
+#print(ty1)
+#y3 = ((ty1/tx1)**x3)
+#print("x1: ", x1, ", ", "y2: ", y1, ", ", "x2: ", x2, ", " "y2: ", y2, ", ", "x3: ", x3, ", " "y3: ", y3)
+#a1 = (y1/(b1**x1))
+#eq1 = (a1*(b2**x2))
+#print(a1)
+#print(eq1)
+if (y1/x1)==(y2/x2): 
+    print("Not Exponential")
+else: 
+    k = (1/(x1-x2)*math.log((1.26/1.414)))
+    A = y2*math.e**(-k*x2)
+    print(k)
+    print(A)
+    y3 = (A*math.e**(k*x3))
+    print("x1: ", x1, ", ", "y2: ", y1, ", ", "x2: ", x2, ", " "y2: ", y2, ", ", "x3: ", x3, ", " "y3: ", y3)
