89b90a8c19d05c8d7e083999616128eaaf62efda
simonwphipps <79718876+simonwphipps@users.noreply.github.com>
2021-03-16 13:44:47 -0400

    Update Ambiguous_Triangular_Solver.py

--- Ambiguous_Triangular_Solver.py
+++ Ambiguous_Triangular_Solver.py
@@ -11,18 +11,25 @@ a1 = radians(ad1)
 
 if s1<(s2*sin(a1)):
     print("There are no solutions")
+    #If side too short to reach the base, no solutions
 elif s1>s2:
+    #Side one is longer than side two there can be only one solution
     print("There is one solution")
     ad2=degrees(asin(s2*sin(a1)/s1))
     print("Angle B is",ad2)
+    #solve angle with law of sines
     ad3=180-ad1-ad2
     print("Angle C is",ad3)
+    #solve with subtraction 
     s3=(s1*sin(radians(ad3)))/sin(a1)
     print("Side C is", s3)
+    #solve with law of sines
 elif s1>=(s2*sin(a1)) and s1<s2:
+    #Side is between size of height and side 2, so two solutions
     print("There are two solutions")
     ad2=degrees(asin(s2*sin(a1)/s1))
     ad4=180-ad2
+    #solve for two solutions with law 
     print("Angle B is",ad2,"or", ad4)
     ad3=180-ad1-ad2
     ad5=180-ad3
