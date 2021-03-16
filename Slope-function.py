c8c57a8f403f815ad5d7d38579d41c14d76721d1
simonwphipps <79718876+simonwphipps@users.noreply.github.com>
2021-03-16 13:27:11 -0400

    Update Slope-function.py

--- Slope-function.py
+++ Slope-function.py
@@ -6,5 +6,7 @@ x1 = float(input("Enter x1: "))
 y1 = float(input("Enter y1: "))
 x2 = float(input("Enter x2: "))
 y2 = float(input("Enter y2: "))
+#Input point values
 
 print("Your distance is",((y2-y1)**2+(x2-x1)**2))**.5
+#This is the slope formula
