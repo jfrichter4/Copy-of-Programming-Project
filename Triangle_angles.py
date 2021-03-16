89b1adfd2416805e2e5e1746203af7983062f016
simonwphipps <79718876+simonwphipps@users.noreply.github.com>
2021-03-16 13:30:11 -0400

    Update Triangle_angles.py

--- Triangle_angles.py
+++ Triangle_angles.py
@@ -6,9 +6,12 @@ from math import acos, sin, degrees
 s1 = float(input("Enter side 1: "))
 s2 = float(input("Enter side 2: "))
 s3 = float(input("Enter side 3: "))
+#Input side values
 
 print("Angle 1: ", degrees(acos((s1**2-s2**2-s3**2)/(-2*s2*s3))))
 print("Angle 2: ", degrees(acos((s2**2-s1**2-s3**2)/(-2*s1*s3))))
 print("Angle 3: ", degrees(acos((s3**2-s1**2-s2**2)/(-2*s1*s2))))
+#Law of cosines
 
 print("Area: ", .5*s1*s2*sin(acos((s3**2-s1**2-s2**2)/(-2*s1*s2))))
+#.5absinc area formula
