14080831f98356ae9897d87370220e9e6aa127c7
simonwphipps <79718876+simonwphipps@users.noreply.github.com>
2021-03-16 13:38:11 -0400

    Update Linear_equation_program.py

--- Linear_equation_program.py
+++ Linear_equation_program.py
@@ -6,13 +6,17 @@ x1 = float(input("Enter x1: "))
 y1 = float(input("Enter y1: "))
 x2 = float(input("Enter x2: "))
 y2 = float(input("Enter y2: "))
+#Input points
 
 if (y2-y1)==0:
     print("y=",y2)
     print("Your line is horizantal!")
+    #If slope is 0, line is horizantal
 elif(x2-x1)==0:
     print("x=",x2)
     print("Your line is vertical!")
+    #If slope is undefined, line is vertical
 else:
     m = (y2-y1)/(x2-x1)
     print("Your line is", "y-",y1,"=",m,"(x-",x1,")")
+    #If not, solve for slope
