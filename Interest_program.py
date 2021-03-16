85079413f9f9ee1f8cd8e5867c1f012d509b3f35
simonwphipps <79718876+simonwphipps@users.noreply.github.com>
2021-03-16 13:36:23 -0400

    Update Interest_program.py

--- Interest_program.py
+++ Interest_program.py
@@ -7,5 +7,7 @@ A1 = float(input("Enter A1: "))
 P1 = float(input("Enter P1: "))
 n1 = float(input("Enter n1: "))
 r1 = float(input("Enter r1: "))
+#Input values
 
 print("Time:",((log10(A1/P1))/(n1*(log10(1+(r1/n1))))))
+#Compound interest equation solved for time
