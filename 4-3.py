c23f5b3c9121bacd3a5adc38b78d92031f0c7fb9
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2021-03-26 23:19:13 -0400

    Updated from Brython Server: 3/26/2021 11:19:37 PM

--- 4-3.py
+++ 4-3.py
@@ -2,19 +2,19 @@
 # Mar. 22, 21
 # 4.3 Programs with Loops
 # Find all the prime factors of a positive integer or state that the integer is prime. 
-import math
 
-#Gets the number to be determined whether it's prime or composite: 
-#Most of the manipulation the program does is with integers, so I did that from the start: 
+#Gets the number to undergo prime factorization: 
 #I replaced the possible negative with nothing just to make it easier for the user: 
-a = int(input("Enter the number: ").replace("-", ""))
-
-#Produces a sequence of numbers beginning from two to a/2: 
-for b in range(2,int(a/2)+1):
-    #It's composite if the above equation finds a to be divisible by any of the numbers it iterates through:  
-    while (a%b)==0:
-        #Prints b-values: 
-        print("A prime factor: ", b),
-        #Makes the a-value a new one and divides by b, so if the input is 12, ... 
-        #...the a value will go from 12 to 6 to 3: 
-        a = a/b
+a = int(input("Enter the integer: ").replace("-", ""))
+if a == 0 or a == 1: 
+    print("Input was zero or one, therefore not prime.")
+else: 
+    #Produces a sequence of numbers beginning from two to a/2: 
+    for b in range(2,int(a/2)+1):
+        #So long as the remainder is nothing (meaning the number being tried is prime, the following code will be carried out:  
+        while (a%b)==0:
+            #Prints b-values which were found to be prime factors using the above line: 
+            print("A prime factor: ", b),
+            #Makes the a-value a new one and divides by b, so if the input is 12, the a value will go from 12 to 6 to 3: 
+            #Basically has the loop restart: 
+            a = a/b
