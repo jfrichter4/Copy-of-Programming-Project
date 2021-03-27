98aeed85b1323123f8a6436db345e5fc628111d9
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2021-03-26 22:24:26 -0400

    Updated from Brython Server: 3/26/2021 10:24:49 PM

--- Programs-with-harder-Input-and-Output-1.py
+++ Programs-with-harder-Input-and-Output-1.py
@@ -8,16 +8,21 @@ y1 = float(input("Input the y-value of the first point: "))
 x2 = float(input("Input the x-value of the second point: "))
 y2 = float(input("Input the y-value of the second point: "))
 x3 = float(input("Input the x-value of a third point: "))
+#Checks if the two divided are equivalent because if they are, it's not exponential: 
 if (y1/x1)==(y2/x2): 
+    #Carries out intention stated in above comment: 
     print("Not Exponential")
+#If the above aren't equivalent: 
 else: 
+    #Subtracts x2 from x1: 
     c = x2-x1
+    #Divides y2 by y1: 
     d = y2/y1
+    #Exponentiates y2/y1 by 1/c: 
     b = d**(1/c)
+    #Divides y1 by b^x1: 
     a = y1/(b**x1)
+    #Multiplies a by b exponentiated by x3: 
     y3 = a*(b**x3)
-    print("C: ", c)
-    print("D: ", d)
-    print("B: ", b)
-    print("A: ", a)
+    #Prints what the program got for y3: 
     print("This is the y-value of the third point: ", y3)
